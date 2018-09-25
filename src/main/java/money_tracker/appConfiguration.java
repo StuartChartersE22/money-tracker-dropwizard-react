73c3fcbfca7a6bfad852d5e8a8e5a5c983ae209f
Stuart Charters <charters.stuart@gmail.com>
2018-09-25 10:05:17 +0100

    Set up dropwizard framework again

---
+++ src/main/java/money_tracker/appConfiguration.java
@@ -0,0 +1,10 @@
+package money_tracker;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class appConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
